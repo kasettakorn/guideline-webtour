2aaa5b9551cee97757ef9a358281bb04cca8b25b
kasettakorn <kasettakorn_techno@hotmail.com>
2020-10-19 00:01:02 +0700

    Updates

---
+++ precache-manifest.c073001e4b7b7243f8491b92f43aaa14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a6a90086569cb20580a0aefd78679c",
+    "url": "/guideline-webtour/index.html"
+  },
+  {
+    "revision": "c3acc639e6468abd4022",
+    "url": "/guideline-webtour/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "9737c2f8252fe876d2c8",
+    "url": "/guideline-webtour/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c3acc639e6468abd4022",
+    "url": "/guideline-webtour/static/js/2.dbfde112.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/guideline-webtour/static/js/2.dbfde112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9737c2f8252fe876d2c8",
+    "url": "/guideline-webtour/static/js/main.bf54d835.chunk.js"
+  },
+  {
+    "revision": "ad4cf50c75168a2b6c27",
+    "url": "/guideline-webtour/static/js/runtime-main.7c3ab4dc.js"
+  }
+]);
